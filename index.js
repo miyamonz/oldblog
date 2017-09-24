edeb3fa034b36c2ac05eeecccd0173dfd246592c
Koichi Miyamoto <m5813k@gmail.com>
2017-09-24 16:49:06 +0900

    moment 追加して、ファイル作成日見れるようにした

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const fs = require("fs")
 const cheerio = require("cheerio")
+const moment = require("moment")
 
 const template = fs.readFileSync("./src/index.html").toString();
 const $ = cheerio.load(template, { decodeEntities: false  })
-
 
 const blogsToJson = require("./parse")
 const blogDir = "./blog/";
 const jsons = blogsToJson(blogDir);
 jsons.forEach( json => {
-  content = json.lines.join("<br>");
-  $("body").append(`<div class="article ${json.file}">${content}</div>`);
+  const fmtdate = moment(json.created_at).format("YYYY/MM/DD")
+  json.lines.splice(1,0,"posted_at "+fmtdate);
+  const content = json.lines.join("<br>");
+  const text = `<div class="article ${json.file}">${content}</div>`;
+  $("body").append(text);
 } )
 
 try{
--- parse.js
+++ parse.js
@@ -4,8 +4,10 @@ function blogsToJson(dirPath) {
   const files = fs.readdirSync(dirPath);
   return files.reverse().map(file => {
     const content = fs.readFileSync(dirPath + file).toString();
+    const stat = fs.statSync(dirPath + file);
     const json = {
       file,
+      created_at: stat.birthtime,
       lines: content.split("\n")
     }
     return json
