f1d7fa7c04281ddabb601cf66fadf91b7797341e
Koichi Miyamoto <m5813k@gmail.com>
2017-09-24 17:31:09 +0900

    createContent 関数で切り分けた

--- index.js
+++ index.js
@@ -9,14 +9,23 @@ const blogsToJson = require("./parse")
 const blogDir = "./blog/";
 const jsons = blogsToJson(blogDir);
 jsons.forEach( json => {
-  const fmtdate = moment(json.created_at).format("YYYY/MM/DD")
-  json.lines.splice(1,0,"posted_at "+fmtdate);
-  const content = json.lines.join("<br>");
+  const content = createContent(json)
   const text = `<div id="${json.file}" class="article ${json.file}">${content}</div>`;
   $(".articles").append(text);
 
   $("nav").append(`<li><a href="#${json.file}">${json.lines[0]}</a></li>`);
 } )
+
+
+function createContent(json) {
+  const lines = Object.assign([], json.lines);
+  lines[0] = `<h1>${lines[0]}</h1>`
+
+  const fmtdate = moment(json.created_at).format("YYYY/MM/DD")
+  lines.splice(1,0,"posted_at "+fmtdate);
+  
+  return lines.join("<br>");
+}
 
 try{
   fs.statSync("./dst")
